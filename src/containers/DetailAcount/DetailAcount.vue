5bb139b92ca7f7b2a66361611ee050bc1d1648ce
lude1994 <1240888747@qq.com>
2018-09-21 18:12:42 +0800

    增加折现

--- src/containers/DetailAcount/DetailAcount.vue
+++ src/containers/DetailAcount/DetailAcount.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="payfor">
+  <div class="detailacount">
     <div class="header">
       <span><---</span>
     </div>
@@ -7,7 +7,7 @@
       2018年9月22日
     </div>
     <div style="background: #425073;height: 83vh;border-radius: 20px 20px 0 0 " class="box">
-
+        <div id="linezhe" :style="{width: '300px', height: '300px'}"></div>
     </div>
 
 
@@ -15,6 +15,8 @@
 </template>
 
 <script>
+  // 引入基本模板
+  let echarts = require('echarts/lib/echarts')
     export default {
         name: "DetailAcount",
       data(){
@@ -22,12 +24,112 @@
 
           }
       },
-      methods: {}
+      methods: {
+          drawLine(){
+            // 基于准备好的dom，初始化echarts实例
+            var myChart = this.$echarts.init(document.getElementById('linezhe'));
+
+            // 绘制图表
+            myChart.setOption({
+              tooltip : {
+                trigger: 'axis',
+                axisPointer: {
+                  type: 'cross',
+                  label: {
+                    backgroundColor: '#6a7985'
+                  }
+                }
+              },
+              legend: {
+                data:['消息','充值']
+              },
+              toolbox: {
+                feature: {
+                  saveAsImage: {}
+                }
+              },
+              grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                // containLabel: true,
+              },
+              xAxis : [
+                {
+                  type : 'category',
+                  boundaryGap : false,
+                  data : ['周一','周二','周三','周四','周五','周六','周五','周六','周日']
+                }
+              ],
+              yAxis : [
+                {
+                  show:false,
+                  type : 'value',
+
+                }
+              ],
+              series : [
+                {
+                  name:'消息',
+                  type:'line',
+                  stack: '总量',
+                  smooth:true,
+                  itemStyle: {
+                    normal: {
+                      color: '#d68262'
+                    }
+                  },
+                  areaStyle: {
+                    normal: {
+                      color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                        offset: 0,
+                        color: '#8ec6ad'
+                      }, {
+                        offset: 1,
+                        color: '#ffe'
+                      }])
+                    }
+                  },
+                  data:[120, 132, 101, 134, 90, 230, 210]
+                },
+                {
+                  name:'充值',
+                  type:'line',
+                  stack: '总量',
+                  smooth:true,
+                  itemStyle: {
+                    normal: {
+                      color: '#d68262'
+                    }
+                  },
+                  areaStyle: {
+                    normal: {
+                      color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                        offset: 0,
+                        color: '#d68262'
+                      }, {
+                        offset: 1,
+                        color: '#ffe'
+                      }])
+                    }
+                  },
+                  data:[220, 182, 191, 234, 290, 330, 310]
+                }
+
+
+              ]
+            })
+
+          }
+      },
+      mounted() {
+          this.drawLine()
+      }
     }
 </script>
 
 <style scoped lang="scss">
-.payfor{
+.detailacount{
   background: url("../../assets/img/bg.jpg") no-repeat;
   background-size: 100%;
   .header{
@@ -38,7 +140,7 @@
     font-size: 20px;
   }
   .box{
-  
+
   }
   .sel{
     padding: 2vh 4vw;
